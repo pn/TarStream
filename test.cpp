a264ee16f48cbb09f8387825e3319e16ce5b4347
Paweł Nadolski <pawel.nadolski@gmail.com>
2011-01-20 23:28:15 +0100

    add writing tar file to disk

--- test.cpp
+++ test.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
+#include <unistd.h>
+#include <fcntl.h>
 #include "TarStream.h"
+#include "stdio.h"
 
 using namespace std;
 
@@ -17,5 +20,17 @@ int main(int argc, char *argv[])
 		files.push_back((string)argv[i]);
 	}
 	TarStream tar((string)argv[1], files);
+
+	int f = open("tmp.tar", O_WRONLY);
+	int i;
+	for (i = 0; i < tar.getSize(); i+=1024)
+	{
+		string s = tar.getChunk(i, 1024);
+		if (tar.getSize() - i >= 1024)
+			write(f, (void*)s.c_str(), 1024);
+		else
+			write(f, (void*)s.c_str(), tar.getSize() - i);
+	}
+	close(f);
 	return 0;
 }
